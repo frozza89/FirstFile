5ee37470c5e8408d0ae69798d53e0f019815166d
Rafael Thiago <rafaor7r@gmail.com>
2014-03-19 21:22:11 -0400

    adding files

---
+++ WebApplication7/WebApplication7/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication7.Models;
+
+namespace WebApplication7.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SearchPage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Search search)
+        {
+            if ()
+            {
+                return ViewBag.Error = "Nada encontrado";
+            }
+            else
+            {
+                return View("SearchPage",search);
+            }
+        }
+
+	}
+}
